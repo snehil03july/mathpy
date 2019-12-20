e94039a11a256e5e5012c962c2b000a7cfdf98dc
RootAndroid58 <rootand58@gmail.com>
2019-12-20 21:20:54 +0530

    many other functions added
    many other functions added !!

--- Trignometry/inversetrigo1.py
+++ Trignometry/inversetrigo1.py
@@ -18,3 +18,41 @@ def arccos_val(r):
 		return p
 	else:
 		print('error')
+		
+		
+
+def arctan_val(r):
+	if r>=-1 and r<=1:
+		p = m.arctan(r)
+		return p
+	else:
+		print('error')
+		
+		
+
+		
+def arccoesc_val(r):
+	if r>=-1 and r<=1:
+		p = m.arccosec(r)
+		return p
+	else:
+		print('error')
+		
+		
+		
+def arcsec_val(r):
+	if r>=-1 and r<=1:
+		p = m.arcsec(r)
+		return p
+	else:
+		print('error')
+		
+		
+		
+def arccot_val(r):
+	if r>=-1 and r<=1:
+		p = m.arccot(r)
+		return p
+	else:
+		print('error')
+
